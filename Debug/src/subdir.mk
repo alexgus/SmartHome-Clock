1172639dcc542cbb27fa09d56ef4d3c8123f5848
alexgus <alex.guyon78@gmail.com>
2015-08-12 13:49:08 +0200

    Added src

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/Clock.cpp \
+../src/Delay.cpp \
+../src/MQTTConnection.cpp \
+../src/Ring.cpp \
+../src/main.cpp 
+
+OBJS += \
+./src/Clock.o \
+./src/Delay.o \
+./src/MQTTConnection.o \
+./src/Ring.o \
+./src/main.o 
+
+CPP_DEPS += \
+./src/Clock.d \
+./src/Delay.d \
+./src/MQTTConnection.d \
+./src/Ring.d \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++1y -I/home/alexandre/Documents/Dev/TO52/org.eclipse.paho.mqtt.c/src -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
